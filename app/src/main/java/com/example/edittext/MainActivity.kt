5b142c501c6ab9289f8de4b908a72fbcb27540ed
Edgar Žigis <tranced.freak@gmail.com>
2021-08-20 22:37:10 +0300

    Upgrade dependencies

--- app/src/main/java/com/example/edittext/MainActivity.kt
+++ app/src/main/java/com/example/edittext/MainActivity.kt
@@ -3,20 +3,24 @@ package com.example.edittext
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.core.content.ContextCompat
 import com.example.edittext.custom.ContextWrapper
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.edittext.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            password.isLight = false
-            password.error = resources.getString(R.string.floating_error_incorrect_password)
-            password.errorColor = ContextCompat.getColor(this, R.color.colorAccent)
+
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.password.isLight = false
+            binding.password.error = resources.getString(R.string.floating_error_incorrect_password)
+            binding.password.errorColor = ContextCompat.getColor(this, R.color.colorAccent)
         }
     }
 
